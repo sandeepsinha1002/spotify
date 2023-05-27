86a86c519e6ba8fd95763b2f3728104a8a844a29
Sandeep Sinha <sinhasandy345@gmail.com>
2023-05-28 02:11:11 +0530

    second

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -55,6 +55,7 @@ export default NextAuth({
         "https://accounts.spotify.com/authorize?scope=user-read-email,playlist-read-private,user-read-email,streaming,user-read-private,user-library-read,user-library-modify,user-read-playback-state,user-modify-playback-state,user-read-recently-played,user-follow-read",
     }),
   ],
+  secret:process.env.NEXT_AUTH_SECRET,
 
   callbacks: {
     async jwt({ token, user, account }) {
